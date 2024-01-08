d25df2f587229874264f020dce5df536d9ad1ba8
Emmanuel Ochoa <eorivera95@gmail.com>
2024-01-08 17:52:36 -0600

    chore: env variables (public are on the client side)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    apiSecret: '123',
+    public: {
+      secretApi: process.env.NUXT_SECRET_API,
+    },
+  },
 })
