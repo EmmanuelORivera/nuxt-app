1de75d97ac828efd054136f3065d225d71ac0380
Emmanuel Ochoa <eorivera95@gmail.com>
2024-01-09 13:48:20 -0600

    feat: example of class and style bindings

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,19 @@
     <AppAlert>Some data</AppAlert>
     <br />
     <button class="btn btn-primary">Click on me</button>
+
+    <div
+      class="static"
+      :class="{ active: isActive, 'text-danger': hasError }"
+    ></div>
+    <div :class="classObject"></div>
   </div>
 </template>
+<script setup lang="ts">
+const isActive = ref(true)
+const hasError = ref(false)
+const classObject = reactive({
+  active: true,
+  'text-danger': false,
+})
+</script>
