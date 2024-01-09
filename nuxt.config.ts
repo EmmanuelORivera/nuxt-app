3ae3e763fbe64ea6ccc5d4a748f355e83b88c5f3
Emmanuel Ochoa <eorivera95@gmail.com>
2024-01-08 18:24:47 -0600

    chore: configuration of bootstrap on the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
       ],
     },
   },
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css', 'bootstrap/dist/css/bootstrap.css'],
   devtools: { enabled: true },
   runtimeConfig: {
     apiSecret: '123',
